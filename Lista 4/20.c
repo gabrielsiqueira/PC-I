6160ccef34e9cdcbd7639b1fa011b7f00ec308ab
DodsTots <rodrigoteshun@gmail.com>
2013-05-16 19:06:01 -0300

    Update 20.c

--- Lista 4/20.c
+++ Lista 4/20.c
@@ -6,9 +6,9 @@ int main()
 {
   int dia,mes,ano1,ano2,x,y,z,diasem,cont;
   cont = 0;
+  printf("Sendo:\n1 - Março, 2 - Abril, 3 - Maio\n4 - Junho, 5 - Julho, 6 - Agosto\n7 - Setembro, 8 - Outubro, 9 - Novembro\n10 - Dezembro, 11 - Janeiro, 12 - Fevereiro\n");
   while(cont<50)
   {
-    printf("Sendo:\n1 - Março | 2 - Abril | 3 - Maio e assim por diante ate 11 - Janeiro | 12 - Fevereiro");
     printf("Digite o dia, o mes e o ano, respectivamente: ");
     scanf("%d %d %d",&dia,&mes,&ano2);
     ano1 = ano2/100;
@@ -19,43 +19,43 @@ int main()
     diasem = (x + dia + ano2 + y + z - 2*ano1)%7;
     if(diasem == 0)
     {
-      printf("DATA LIDA = %d/%d/%d/%d\nDOMINGO",dia,mes,ano1,ano2);
+      printf("\nDATA LIDA = %d/%d/%d%d\nDOMINGO\n\n",dia,mes,ano1,ano2);
     }
     else
     {
       if(diasem == 1)
       {
-        printf("DATA LIDA = %d/%d/%d/%d\nSEGUNDA-FEIRA",dia,mes,ano1,ano2);
+        printf("\nDATA LIDA = %d/%d/%d%d\nSEGUNDA-FEIRA\n\n",dia,mes,ano1,ano2);
       }
       else
       {
         if(diasem == 2)
         {
-          printf("DATA LIDA = %d/%d/%d/%d\nTERÇA-FEIRA",dia,mes,ano1,ano2);
+          printf("\nDATA LIDA = %d/%d/%d%d\nTERÇA-FEIRA\n\n",dia,mes,ano1,ano2);
         }
         else
         {
           if(diasem == 3)
           {
-            printf("DATA LIDA = %d/%d/%d/%d\nQUARTA-FEIRA",dia,mes,ano1,ano2);
+            printf("\nDATA LIDA = %d/%d/%d%d\nQUARTA-FEIRA\n\n",dia,mes,ano1,ano2);
           }
           else
           {
             if(diasem == 4)
             {
-              printf("DATA LIDA = %d/%d/%d/%d\nQUINTA-FEIRA",dia,mes,ano1,ano2);
+              printf("\nDATA LIDA = %d/%d/%d%d\nQUINTA-FEIRA\n\n",dia,mes,ano1,ano2);
             }
             else
             {
               if(diasem == 5)
               {
-                printf("DATA LIDA = %d/%d/%d/%d\nSEXTA-FEIRA",dia,mes,ano1,ano2);
+                printf("\nDATA LIDA = %d/%d/%d%d\nSEXTA-FEIRA\n\n",dia,mes,ano1,ano2);
               }
               else
               {
                 if(diasem == 6)
                 {
-                  printf("DATA LIDA = %d/%d/%d/%d\nSABADO",dia,mes,ano1,ano2);
+                  printf("\nDATA LIDA = %d/%d/%d%d\nSABADO\n\n",dia,mes,ano1,ano2);
                 }
               }
             }
