c2c3d1bbfcc32d09ce8b26495a93177c1dfc98e8
skzt <pedroskzt@gmail.com>
2013-05-17 00:07:57 -0300

    Update 01.c

--- Lista Estruturas de Repetição/01.c
+++ Lista Estruturas de Repetição/01.c
@@ -4,27 +4,33 @@
 #include<stdlib.h>
 int main()
 {
+    /*
+	z= Armazena a soma das idades digitadas.
+	y= Armazena o numero de idades digitadas.
+	*/
     float y, z, idade;
-    idade = 1;
+	idade = 1;//Garante o loop infinito.
     z = 0;
     y = 0;
     while (idade != 0)
     {
-        printf("Digite uma idade: ");
+        printf("Digite uma idade ou 0 para terminar: ");
         scanf("%f",&idade);
-        if(idade != 0)
+        //Verifica quando vai ter que parar o loop.
+		if(idade != 0)
         {
             z = z + idade;
             y = y + 1;
         }
     }
     idade = z/y;
+    //Avisa caso nenhuma idade tenha sido enserida.
     if(y == 0)
     {
         printf("Nenhuma idade foi inserida");
     }
     else
     {
-        printf("A media e: %.2f",idade);
+        printf("A media e: %f",idade);
     }
 }
