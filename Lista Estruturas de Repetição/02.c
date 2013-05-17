40dadd5cf41a144f93839ec2a5f7f013eeee6150
skzt <pedroskzt@gmail.com>
2013-05-17 00:42:27 -0300

    Update 02.c

--- Lista Estruturas de Repetição/02.c
+++ Lista Estruturas de Repetição/02.c
@@ -2,32 +2,41 @@
 
 int main ()
 {
+/*
+k= Numero de mulhers.
+o= Numero de homens.
+h= Altura.
+soma= Soma das alturas das mulheres.
+max= Maior altura.
+min= Menor altura.
+media= Media das alturas.
+*/
 int k=0,i,o=0;
 float h,soma=0,max=0,min=1000,media;
 char sexo[10];
 for(i=0;i<50;i++)
 {
 printf ("Digite a altura e o sexo da pessoa.(M-masculino F-feminino)\n");
-scanf ("%s",sexo);
 scanf ("%f",&h);
-if (h>max)
+scanf ("%s",sexo);
+if (h>max)//Testa todas as alturas digitadas e salva apenas a maior de todas.
 {
 max=h;
 }
-else if (h<min)
+else if (h<min)//Testa todas as alturas digitadas menores que o max e salva apenas a menor de todas.
 {
 min=h;
 }
 
 
-if (sexo[0]=='f' || sexo[0]=='F')
+if (sexo[0]=='f' || sexo[0]=='F')//if para somar apenas a altura das mulheres e contar quantas mulheres foram digitadas.
 {
 soma=soma+h;
 k++;
 }
 else
 {
-if (sexo[0]=='m' || sexo[0]=='M')
+if (sexo[0]=='m' || sexo[0]=='M')//if para contar quantos homens fora digitados.
 {
 o++;
 }
