426815f81265eabb712d6e690768a051f0b08903
Pedro Henrique <phd.pc@hotmail.com>
2013-05-16 23:51:47 -0300

    Update 17.c

--- Lista Estruturas de Repetição/17.c
+++ Lista Estruturas de Repetição/17.c
@@ -1,21 +1,31 @@
 #include <stdio.h>
 
-int main ()
+int main( void )
 {
-  float lucro,preco,ingresso,maxl=0/*Lucro maximo*/,maxp=0/*Preço do ingresso quando o lucro é maximo.*/,maxi/*Numero de ingressos vendidos qnd o lucro é maximo.*/;
-	ingresso=120;
-	preco=5.00;
-	do{
-		lucro=(preco*ingresso)-200;
-		if (lucro>maxl)//Testa todos os valores de lucro e deixa salvo em maxl,maxi e maxp apenas os valores do maior lucro, dos ingressos referentes a esse lucro e o preco dos ingressos para esse lucro,respectivamente.
+	//	max1 = Lucro maximo
+	//	maxp = Preço do ingresso quando o lucro é maximo
+	//	maxi = Numero de ingreços vendidos qnd o lucro é maximo
+	float lucro, preco = 5.0, ingresso = 120.0, maxl = .0, maxp = .0, maxi;
+
+	do
+	{
+		lucro = preco * ingresso - 200;
+
+	// Testa todos os valores de lucro e deixa salvo em maxl,maxi e maxp apenas 
+	// os valores do maior lucro, dos ingressos referentes a esse lucro e o preco
+	// dos ingressos para esse lucro,respectivamente.
+	if ( lucro > maxl )
 		{
-			maxl=lucro;
-			maxi=ingresso;
-			maxp=preco;
+			maxl = lucro;
+			maxi = ingresso;
+			maxp = preco;
 		}
-		printf ("Lucro= %f, Lucro esperado= %f.\n",preco,lucro);
-		preco-=0.5;
-		ingresso+=26;
-	}while(preco>=1);
-	printf ("Lucro maximo= %f ao preco= %f vendendo %f ingressos.\n",maxl,maxp,maxi);
+
+		printf( "Lucro= %f, Lucro esperado= %f.\n", preco, lucro );
+
+		preco -= 0.5;
+		ingresso += 26.0;
+	} while ( preco >= 1.0 );
+
+	printf( "Lucro maximo= %f ao preco= %f vendendo %f ingressos.\n", maxl, maxp, maxi );
 }
