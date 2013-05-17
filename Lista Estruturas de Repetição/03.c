3b96518952903c3ba858df6a968b1bd6891ce009
skzt <pedroskzt@gmail.com>
2013-05-17 20:56:25 -0300

    Update 03.c

--- Lista Estruturas de Repetição/03.c
+++ Lista Estruturas de Repetição/03.c
@@ -5,11 +5,11 @@
 int main()
 {
     float C,F;
-    F = 50;
+    F = 50;//Temperatura inicial em farenheits.
     while(F<151)
     {
-        C = (5*(F-32))/9;
+        C = (5*(F-32))/9;//Calculo da temperatura em graus celcius.
         printf("Valor de F = %1.f Valor de C = %.2f\n",F,C);
-        F = F + 1;
+        F = F + 1;//Aumento de 1grau farenheits por volta do loop.
     }
 }
