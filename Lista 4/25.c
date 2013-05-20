4cb22bf887c342f1230f435ac4848a4b28a5cea9
skzt <pedroskzt@gmail.com>
2013-05-20 15:22:26 -0300

    Update 25.c

--- Lista 4/25.c
+++ Lista 4/25.c
@@ -4,16 +4,13 @@
 
 int main ()
 {
-  int n1,n2=50;
+	int n1,n2=50;
 	float S=0,D,d;	
 	for (n1=1;n1<51;n1++)
 	{
 	D=pow(2,n1);
-	printf ("D=%f",D);
 	d=n2;
-	printf ("d=%f",d);
 	S=S+(D/d);
-	printf ("S=%f",S);
 	n2--;
 	}
 	printf ("\nS=%f\n",S);
