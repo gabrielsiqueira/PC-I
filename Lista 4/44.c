eca705352df618a4733781b523bceed414e74ca0
DodsTots <rodrigoteshun@gmail.com>
2013-05-24 18:22:25 -0300

    Update 44.c

---
+++ Lista 4/44.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<math.h>
+#include<string.h>
+int main()
+{
+  float a,b,A;
+  while(a!=0 && b!=0)
+  {
+    printf("Digite os valores de A e B, respectivamente: ");
+    scanf("%f %f",&a,&b);
+    if(a!=0 && b!=0)
+    {
+      A = (b*a)/2;
+      printf("Valor de A: %g\nValor de B: %g\nValor da area: %g",a,b,A);
+    }
+    else
+    {
+      printf("Sessão finalizada.")
+    }
+  }
+}
