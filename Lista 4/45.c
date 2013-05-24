2f48e4890e904737061c863dc76caf5f54a1beab
DodsTots <rodrigoteshun@gmail.com>
2013-05-24 18:43:24 -0300

    Update 45.c

---
+++ Lista 4/45.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<math.h>
+#include<string.h>
+int main()
+{
+  int x,y;
+  x = 1;
+  while(x < 11)
+  {
+    if(x < 4)
+    {
+      printf(Y == 1);
+    }
+    else
+    {
+      if(x == 4)
+      {
+        printf("Y = 32");
+      }
+      else
+      {
+        if(x > 4)
+        {
+          y = pow(x,2) - 16;
+          printf("Y = %g",y);
+        }
+      }
+      x = x + 1;
+    }
+  }
