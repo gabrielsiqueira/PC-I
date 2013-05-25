a442a5e50e865765881f14bc5bfa51d754f321c1
Mickaella <mickaella2@hotmail.com>
2013-05-25 17:48:14 -0300

    Update 15.c

---
+++ Lista 4/15.c
@@ -0,0 +1,89 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+//Os bancos atualizam diariamente as contas de seus clientes.
+//Essa atualização envolve a análise dos depósitos e retiradas de cada conta.
+//Numa conta de saldo mínimo, uma taxa de serviço é deduzida se a conta cai abaixo
+//de certa quantia especificada.
+//A primeira linha contem o numero da conta, o valor do saldo atual
+//e do saldo mínimo diário, quantidade de transações e taxa de serviço;
+//As linhas seguintes contem o valor e o código da transação (depósito ou retirada);
+//Calcule o saldo (saldo/débito) da conta ao fim do dia
+// (se o resultado for negativo, isto
+//significa insuficiência de fundos na conta );
+//Escreva, para cada conta, o seu número e o saldo calculado.
+// Se não houver fundos, imprima número da conta e a mensagem “ NÃO HÁ FUNDOS ”;
+//Utilize como flag o número da conta igual a zero;
+
+int main (void)
+{
+  // Declaração das variáveis:
+
+    float minimo;// balanco minimo diario
+	int trans = 0;// quantidade de transacoes do dia
+	float taxa;// valor da taxa de servico
+	int conta;// numero da conta
+	float valor;// valor da transacao
+	char tipo;// tipo da transacao (d-deposito, r-retirada)
+	float saldo;// saldo da conta
+
+	// Leitura das condições do banco:
+
+    printf ( "\nInforme o numero da conta:\n");
+    scanf (" %d", &conta);
+
+    while (conta!=0)
+     {
+	printf ("\nInforme o valor do saldo atual:\n");
+	scanf (" %f", &saldo);
+
+	printf ("\nInforme o valor do balanco minimo diario:\n");
+	scanf (" %f", &minimo);
+
+	printf ("\nInforme a quantidade de transacoes:\n");
+	scanf(" %d", &trans);
+
+
+	printf ("\nInforme o valor da taxa de servico (em porcento):\n");
+	scanf(" %f", &taxa);
+
+	while(trans>0)
+	{
+		// Leitura dos valores das contas:
+
+		printf ("\nInforme o valor da transacao:\n");
+		scanf ("%f", &valor);
+
+		printf ("\nInforme o tipo da transacao [D = Deposito| R = Retirada]:\n");
+		scanf (" %c", &tipo);
+
+		// Verifica se é uma operação de depósito ou retirada:
+
+		if ( tipo == 'D' )
+		{
+			saldo += valor;
+		}
+		else
+		{
+			if ( tipo == 'R' )
+			{
+				saldo -= valor;
+			}
+		}
+		trans--;
+	}
+
+//Agora decide o que será mostrado:
+
+	if ( saldo < minimo )
+		{
+			saldo = saldo - saldo * taxa / 100;
+
+			printf ("\nNUMERO DA CONTA: %d.\nNAO HA FUNDOS\n", conta);
+		}
+		else
+		{
+			printf ("\nNUMERO DA CONTA: %d.\nSALDO FINAL: %.2f.\n", conta, saldo);
+		}
+     }
+}
