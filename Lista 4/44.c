b0a245d7a62b55008f4c59e5cbda3a104b32cd20
DodsTots <rodrigoteshun@gmail.com>
2013-05-26 13:46:50 -0300

    Update 44.c

--- Lista 4/44.c
+++ Lista 4/44.c
@@ -4,19 +4,33 @@
 #include<string.h>
 int main()
 {
-  float a,b,A;
-  while(a!=0 && b!=0)
-  {
-    printf("Digite os valores de A e B, respectivamente: ");
-    scanf("%f %f",&a,&b);
-    if(a!=0 && b!=0)
+    float a,b,A;
+    while(a+b != 0)
     {
-      A = (b*(-b/a))/2;
-      printf("Valor de A: %g\nValor de B: %g\nValor da area: %g",a,b,A);
+        printf("Digite os valores de A e B, respectivamente: ");
+        scanf("%f %f",&a,&b);
+        if(a+b != 0)
+        {
+            if(a != 0)
+            {
+                A = (b*(-b/a))/2;
+                if(A == -0)
+                {
+                    printf("Valor de A: %g\nValor de B: %g\nValor da area: 0\n",a,b);
+                }
+                else
+                {
+                    printf("Valor de A: %g\nValor de B: %g\nValor da area: %g\n",a,b,A);
+                }
+            }
+            else
+            {
+                printf("Nao existe triangulo.\n");
+            }
+        }
+        else
+        {
+            printf("Sessão finalizada.");
+        }
     }
-    else
-    {
-      printf("Sessão finalizada.")
-    }
-  }
 }
