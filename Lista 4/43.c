c2eed7ec1fe9d9cf883ccc474c25f9686ccbf943
DodsTots <rodrigoteshun@gmail.com>
2013-05-26 13:45:20 -0300

    Update 43.c

--- Lista 4/43.c
+++ Lista 4/43.c
@@ -5,18 +5,18 @@
 int main()
 {
   float a,b,c,d,x,y,u,v;
-  while(a!=0 && b!=0 && c!=0 && d!=0)
+  while(a+b+c+d != 0)
   {
     printf("Digite os valores de a, b, c, d, u e v, respectivamente: ");
     scanf("%f %f %f %f %f %f",&a,&b,&c,&d,&u,&v);
     if(((a*d) - (b*c)) == 0)
     {
-      printf("Para:\na = %g\nb = %g\nc = %g\nd = %g\nu = %g\nv = %g\nNão existe x nem y, digite novamente.\n--------\n",a,b,c,d,u,v);
+      printf("Para:\na = %g\nb = %g\nc = %g\nd = %g\nu = %g\nv = %g\nNão existe x nem y.\n--------\n",a,b,c,d,u,v);
     }
     else
     {
-      x = ((d/((a*d)-(b*c)))*u) - ((b/(a*d)(b*c))*v);
-      y = ((-c/((a*d)-(b*c)))*u) - ((a/(a*d)(b*c))*v);
+        x = ((d/((a*d)-(b*c)))*u) - ((b/(a*d)-(b*c))*v);
+      y = ((-c/((a*d)-(b*c)))*u) - ((a/(a*d)-(b*c))*v);
       printf("Para:\na = %g\nb = %g\nc = %g\nd = %g\nu = %g\nv = %g\n\nx = %g\ny = %g\n--------\n",a,b,c,d,u,v,x,y);
     }
   }
