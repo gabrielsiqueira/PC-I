bc4334f561040aa79817607686193425d211d2b2
Paulo <paulomartins_88@hotmail.com>
2013-05-28 00:42:13 -0300

    Update 64.c

---
+++ Lista 4/64.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+
+{
+  int num,i,soma;
+
+    for (num = 1; num <= 100; num++)
+    {
+        for (soma = 0, i = 1; i < num; i++)
+    {
+
+        if (num % i == 0)
+        soma += i;
+    }
+
+            if (soma == num)
+            printf("Numero perfeito: %d\n",num);
+    }
+
+    return 0;
+}
